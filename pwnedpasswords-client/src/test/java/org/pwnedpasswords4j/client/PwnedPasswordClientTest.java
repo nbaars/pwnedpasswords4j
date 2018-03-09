eb1a53eea338b5b64ae988214165b4d5955831ac
Nanne Baars <nanne.baars@owasp.org>
2018-03-09 10:07:31 +0100

    Added async way of calling the client (need to rewrite the ok http client)

--- pwnedpasswords-client/src/test/java/org/pwnedpasswords4j/client/PwnedPasswordClientTest.java
+++ pwnedpasswords-client/src/test/java/org/pwnedpasswords4j/client/PwnedPasswordClientTest.java
@@ -1,12 +1,6 @@
 package org.pwnedpasswords4j.client;
 
 import org.junit.Test;
-import org.mockito.Matchers;
-import org.mockito.Mockito;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
 
 public class PwnedPasswordClientTest {
 
