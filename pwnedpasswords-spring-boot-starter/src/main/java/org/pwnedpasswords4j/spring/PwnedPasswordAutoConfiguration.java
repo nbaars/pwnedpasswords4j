f232d56d22cf60d375504ce22718f08d73ad1395
Nanne Baars <nanne.baars@owasp.org>
2018-03-19 14:17:22 +0100

    Removed conditional for auto configuration

--- pwnedpasswords-spring-boot-starter/src/main/java/org/pwnedpasswords4j/spring/PwnedPasswordAutoConfiguration.java
+++ pwnedpasswords-spring-boot-starter/src/main/java/org/pwnedpasswords4j/spring/PwnedPasswordAutoConfiguration.java
@@ -3,15 +3,12 @@ package org.pwnedpasswords4j.spring;
 import okhttp3.OkHttpClient;
 import org.pwnedpasswords4j.client.PwnedPasswordChecker;
 import org.pwnedpasswords4j.client.PwnedPasswordClient;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration.RestTemplateConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(PwnedPasswordProperties.class)
-@ConditionalOnClass(RestTemplateConfiguration.class)
 public class PwnedPasswordAutoConfiguration {
 
     @Bean
